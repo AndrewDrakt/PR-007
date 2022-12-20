9f6cfdcf9ae970386e3500594f3ff6ac02b5f01f
AndrewDrakt <59191311+AndrewDrakt@users.noreply.github.com>
2022-12-20 16:41:25 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace практ7
+{
+    internal class strelk
+    {
+        public static void dd1(int b)
+        {
+            
+        }
+        public static void dd2(int b)
+        {
+           
+        }
+    }
+}
